123932282e734f21b62131c0bb6ec3162f6e9c45
mahshidghaffari <m.ghaffari@student.maastrichtuniversity.nl>
2021-10-13 13:48:54 +0200

    add doc comment in player and dicce class

--- app/src/main/java/controller/Dice.java
+++ app/src/main/java/controller/Dice.java
@@ -2,14 +2,29 @@ package controller;
 import java.util.Random;
 
 
+/**
+
+*	Dice class generate random number between 1 to 6 
+* @return      a string or number  
+*/
+
 public class  Dice {
     private String [] diceName = {"Pawn","Knight","Bishop","Rook","Queen","King"};
     private Random rand = new Random();
     private int diceNumber;
     
+    
+    /**
+    * @return an integer number between 1 to 6  
+    */
+    
     public int getNumberDice(){
         return diceNumber;
     }
+    
+    /**
+     * @return String , name of the each piece name which it's equivalents to its number
+     */
 
     public String getRoleDice(){
         return diceName[diceNumber];
