d6344c76dd524f52d2fa058f08bd2b3cd2069588
mahshidghaffari <m.ghaffari@student.maastrichtuniversity.nl>
2021-11-04 13:09:14 +0100

    bas

--- app/src/main/java/controller/Game.java
+++ app/src/main/java/controller/Game.java
@@ -23,7 +23,6 @@ public class Game{
     private boolean showCastleButton= false;
     private JButton castleButton;
     private boolean diceClicked=false;
-    private boolean BaseLIneAgentActive = true;
     
     
     public Game(){
@@ -33,19 +32,22 @@ public class Game{
         wPlayer = new WhitePlayer(cb);
 
     }
-    
-//    bugs
-//    1- this method is call after white!
-//	  2- dice is not changing the turn (i.e black get chance to roll dice twice)
+    public ChessBoard getChessBoard(){
+        return cb;
+    }
+    public boolean isNewTurn(){
+        return newTurn;
+    }
+    public Dice getDice(){
+        return dice;
+    }
+    public Player whosPlaying(){
+        return playing;
+    }
     
     public boolean AIplay(String piceName) {
     	System.out.println("AI is playing ");
-//    	diceClicked=true;
-//        this.setDiceClicked(true);
-//        this.getDice().randomize();
-//        String piceName =  piceName;
         Random rand = new Random();
-//        Random rand1 = new Random();
         updateBoard();
 
     	System.out.println(piceName);
@@ -62,8 +64,6 @@ public class Game{
      		
      		for (Piece piece : movablePieces) {
      			ArrayList<Square> legalMovesForPiece = piece.getLegalMoves(cb);
-     			//legalMoves.addAll(piece.getLegalMoves(cb));
-     			//System.out.println(piece.getLegalMoves(cb));
      			for(Square legalMove : legalMovesForPiece) {
      				legalMoves.add(legalMove);
      				pieceToMove.add(piece);
@@ -77,18 +77,6 @@ public class Game{
      	return true;
     }
     
-    public ChessBoard getChessBoard(){
-        return cb;
-    }
-    public boolean isNewTurn(){
-        return newTurn;
-    }
-    public Dice getDice(){
-        return dice;
-    }
-    public Player whosPlaying(){
-        return playing;
-    }
     public void newTurn(){
         wPlayer.flipTurns(bPlayer);
         newTurn=true;
@@ -115,7 +103,7 @@ public class Game{
     public void play(){
         
         if(wPlayer.getIsMyTurn()){        //if its w player's turn
-            // playing = wPlayer;
+            playing = wPlayer;
             newTurn= false;
             String chosen = dice.getRoleDice(); //roll the dice
             if(!wPlayer.canMove(chosen)){         //if player has no pieces to move we switch turns
@@ -124,7 +112,7 @@ public class Game{
             }
         }   
         else if(bPlayer.getIsMyTurn()){
-            // playing = bPlayer;
+            playing = bPlayer;
             newTurn= false;
             String chosen = dice.getRoleDice();
             //loop through all live pieces to see if dice chosen piece piece is there
@@ -139,8 +127,7 @@ public class Game{
                 System.out.println("Sorry black , you have no possible moves. Turn goes to white");     
                 newTurn();
             }else {
-            	AIplay(chosen);
-            	
+            	this.AIplay(chosen);
             }
         }
     }
@@ -149,8 +136,8 @@ public class Game{
     public boolean isLegalChoice(boolean clickedOnce, SquareButton clickedButton){
         Square clickedSquare = cb.getSquare(clickedButton); //get clicked square 
         Piece clickedPiece = clickedSquare.getPieceOnSq();
-        if(wPlayer.getIsMyTurn()){ //if its the white turnc
-        	System.out.println("0");
+        
+        if(wPlayer.getIsMyTurn()){ //if its the white turn
             if(!diceClicked){ return false;}
             if(!clickedOnce){ //and this click is the choice of which piece to move 
                 if(!clickedSquare.isTakenSquare()){ return false; }    //if the sqaure is empty then do nothing
@@ -182,25 +169,15 @@ public class Game{
                             Piece rook = cb.getBoard()[7][0].getPieceOnSq();
                             rook.move(cb.getBoard()[7][3],cb,rook.getLegalMoves(cb));
                         }
-                        
-//                        if (BaseLIneAgentActive) {
-//                        	AIplay();
-//                        }else {
-                        	newTurn();                        	
-//                        }
-                        
+
+                        newTurn();
                         return true;
-                     
                     }
 
                     else{  //if its just a non castling move
                         heldPiece.move(clickedSquare, cb, heldPiece.getLegalMoves(cb)); //move there    
                         System.out.println("legal second click");
-//                        if (BaseLIneAgentActive) {
-//                        	AIplay();
-//                        }else {
-                        	newTurn();                        	
-//                        }
+                        newTurn();
                         return true;
                     }                    
                 }
@@ -208,57 +185,49 @@ public class Game{
         }
 
         else if(bPlayer.getIsMyTurn()){ //if its the black turn
-        	System.out.println(BaseLIneAgentActive);
-        	if (BaseLIneAgentActive) {
-        		System.out.println("lalalalal");
-//        		AIplay();
-        		return true;
-        	}else {
-        		System.out.println("lilililili");
-                if(!diceClicked){ return false;}
-                if(!clickedOnce){ //and this click is the choice of which piece to move 
-                    if(!clickedSquare.isTakenSquare()){ return false; }    //if the sqaure is empty then do nothing
-                    else if (!clickedPiece.isWhite() && clickedPiece.getPieceName().equals(dice.getRoleDice()) ){ //if the player selected the correct piece to move
-                        heldPiece = clickedPiece; 
-                        System.out.println("legal first click");
-                        return true;
-                   }
+            if(!diceClicked){ return false;}
+            if(!clickedOnce){ //and this click is the choice of which piece to move 
+                if(!clickedSquare.isTakenSquare()){ return false; }    //if the sqaure is empty then do nothing
+                else if (!clickedPiece.isWhite() && clickedPiece.getPieceName().equals(dice.getRoleDice()) ){ //if the player selected the correct piece to move
+                    heldPiece = clickedPiece; 
+                    System.out.println("legal first click");
+                    return true;
+               }
+            }
+            else{ //if this is the second click
+                if(heldPiece!=null && cb.getSquare(clickedButton).equals(heldPiece.getCurrentPosition())){   //if the player wants to move another piece
+                    heldPiece=null;
+                    clickedOnce = false;
+                    System.out.println("released");
+                    return true;
                 }
-                else{ //if this is the second click
-                    if(heldPiece!=null && cb.getSquare(clickedButton).equals(heldPiece.getCurrentPosition())){   //if the player wants to move another piece
-                        heldPiece=null;
-                        clickedOnce = false;
-                        System.out.println("released");
+                else if(heldPiece!=null && heldPiece.getLegalMoves(cb).contains(clickedSquare)){ //if the clicked square is in fact a legal one to move to
+                    
+                    int distance = Math.abs(clickedSquare.getXPos()-heldPiece.getCurrentPosition().getXPos());    //calculating the distance from the piece to desired moving location                
+                    if(heldPiece.getPieceName().equals("King") && distance>1){                  //if the size of this movement is larger than 1 for the king it means this is castling
+                        heldPiece.move(clickedSquare,cb,heldPiece.getLegalMoves(cb));           //move the king to desired location
+
+                        if(clickedSquare.getXPos()>4){             //if it is  a close castling
+                            Piece rook = cb.getBoard()[0][7].getPieceOnSq();
+                            rook.move(cb.getBoard()[0][5], cb, rook.getLegalMoves(cb));  //move the rook to correct location next to king
+                        }
+
+                        else {                                      //if it is a far castling
+                            Piece rook = cb.getBoard()[0][0].getPieceOnSq();
+                            rook.move(cb.getBoard()[0][3],cb,rook.getLegalMoves(cb));   //move the rook to correct location next to king
+                        }
+                        newTurn();
                         return true;
                     }
-                    else if(heldPiece!=null && heldPiece.getLegalMoves(cb).contains(clickedSquare)){ //if the clicked square is in fact a legal one to move to
-                        
-                        int distance = Math.abs(clickedSquare.getXPos()-heldPiece.getCurrentPosition().getXPos());    //calculating the distance from the piece to desired moving location                
-                        if(heldPiece.getPieceName().equals("King") && distance>1){                  //if the size of this movement is larger than 1 for the king it means this is castling
-                            heldPiece.move(clickedSquare,cb,heldPiece.getLegalMoves(cb));           //move the king to desired location
 
-                            if(clickedSquare.getXPos()>4){             //if it is  a close castling
-                                Piece rook = cb.getBoard()[0][7].getPieceOnSq();
-                                rook.move(cb.getBoard()[0][5], cb, rook.getLegalMoves(cb));  //move the rook to correct location next to king
-                            }
-
-                            else {                                      //if it is a far castling
-                                Piece rook = cb.getBoard()[0][0].getPieceOnSq();
-                                rook.move(cb.getBoard()[0][3],cb,rook.getLegalMoves(cb));   //move the rook to correct location next to king
-                            }
-                            newTurn();
-                            return true;
-                        }
-
-                        else{  //if its just a non castling move
-                            heldPiece.move(clickedSquare, cb, heldPiece.getLegalMoves(cb)); //move there
-                            newTurn();
-                            System.out.println("legal second click");
-                            return true;                    
-                        }
+                    else{  //if its just a non castling move
+                        heldPiece.move(clickedSquare, cb, heldPiece.getLegalMoves(cb)); //move there
+                        newTurn();
+                        System.out.println("legal second click");
+                        return true;                    
                     }
-                } 
-        	}
+                }
+            }    
         }
         return false;
     }
