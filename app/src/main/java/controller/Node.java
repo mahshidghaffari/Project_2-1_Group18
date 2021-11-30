d47b293d5bc6259f6c4c72f3daede3aa6624950e
Ben Elis <67229567+GUNELIS@users.noreply.github.com>
2021-11-30 19:00:48 +0100

    alpha

--- app/src/main/java/controller/Node.java
+++ app/src/main/java/controller/Node.java
@@ -8,6 +8,9 @@ public class Node {
     private ArrayList<Node> children;
     private ChessBoard board;
     private String piece;
+    private double alpha;
+    private double beta;
+    
 
     public Node(boolean isProbability, String piece, ChessBoard board) {
         this.isProbability = isProbability;
@@ -27,6 +30,18 @@ public class Node {
         return board;
     }
 
+    public double getAlpha(){
+        return alpha;
+    }
+    public double getBeta(){
+        return beta;
+    }
+    public void setAlpha(double alpha){
+        this.alpha=alpha;
+    }
+    public void setBeta(double beta){
+        this.beta = beta;
+    }
     public void setBoard(ChessBoard board) {
         this.board = board;
     }
